d86890b48c007d336d6a80785f45a423ef5cb36b
max 96 <119216428+max96git@users.noreply.github.com>
2023-05-21 02:09:03 -0700

    Update SocialRainbow.lua

--- SocialRainbow.lua
+++ SocialRainbow.lua
@@ -1,10 +1,11 @@
 --[[
-	SocialRainbow by VSCPlays (originally from @kernelvox (max96git))
+	<SocialRainbow by VSCPlays (originally from @kernelvox (max96git))
 	This rainbow module is based on @WinnersTakeAll (RyanLua)'s Shime
 	I made this for @kernelvox as @bluebxrrybot and @commitblue said it's not enough for a community resource,
 	I want to help @kernelvox succed
-
 	please credit me @VSCPlays for the module and @kernelvox for the idea
+	
+	The Shime Owner suggested some stuff, so I added some adjustments
 ]]
 
 --// Services \\--
@@ -12,13 +13,15 @@ local RunService = game:GetService("RunService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 --// Sanity Checks \\--
-if not RunService:IsClient() then
-	error(`Please use require({script:GetFullName()}) in the client`)
-end
 
 if not script:IsDescendantOf(ReplicatedStorage) then
 	error(`Please put this in {ReplicatedStorage:GetFullName()}, where both the client and the server can access it`)
 end
+
+--// Bindables \\--
+local event1 = Instance.new("BindableEvent")
+local event2 = Instance.new("BindableEvent")
+local event3 = Instance.new("BindableEvent")
 
 --// rainbow \\--
 local rainbow = {}
@@ -29,19 +32,10 @@ function rainbow.new(item:GuiObject, it:"Border" | "Text" | "Background", cooldo
 
 	self.itemType = it
 	self.item = item
-	self.isPlaying = false
-	self.isPaused = false
-	self.finished = false
+	self.playbackState = Enum.PlaybackState.Completed
 	self.cooldown = cooldown or 0
-
-	return self
-end
-
-function rainbow:Play()
-	self.isPlaying = true
-	self.isPaused = false
-	self.finished = false
-	RunService:BindToRenderStep("RainbowEffect" ..self.item.Name, 1, function()
+	self.func = function()
+		self.playbackState = Enum.PlaybackState.Playing
 		local color = Color3.fromHSV((tick() * 4) % 1, 1, 1)
 		if self.itemType == "Border" then
 			self.item.BorderColor3 = color
@@ -53,25 +47,69 @@ function rainbow:Play()
 			self.item.BackgroundColor3 = color
 		end
 		task.wait(self.cooldown)
-	end)
+	end
+	self.Played = event1.Event
+	self.Paused = event2.Event
+	self.Finished = event3.Event
+
+	return self
 end
 
-function rainbow:Pause()
-	if self.finished then
-		return
+function rainbow:Play()
+	event1:Fire(self.item, self.playbackState)
+	self.playbackState = Enum.PlaybackState.Begin
+	
+	self.func = function()
+		self.playbackState = Enum.PlaybackState.Playing
+		local color = Color3.fromHSV((tick() * 4) % 1, 1, 1)
+		if self.itemType == "Border" then
+			self.item.BorderColor3 = color
+		elseif self.itemType == "Text" then
+			if self.item.ClassName == "TextBox" or "TextButton" or "TextLabel" then
+				self.item.TextColor3 = color
+			end
+		elseif self.itemType == "Background" then
+			self.item.BackgroundColor3 = color
+		end
+		task.wait(self.cooldown)
 	end
 	
-	self.isPlaying = false
-	self.isPaused = true
-	self.finished = false
-	RunService:UnbindFromRenderStep("RainbowEffect" ..self.item.Name)
+	while true do
+		if self.func == nil then
+			break
+		end
+		
+		self.func()
+	end
+end
+
+function rainbow:Pause(seconds:number)
+	if self.playbackState ~= Enum.PlaybackState.Playing then
+		return
+	end
+
+	if seconds == self.cooldown then
+		seconds += self.cooldown
+	end
+
+	event2:Fire(self.item, self.playbackState, seconds)
+
+	seconds = math.clamp(seconds, 0.1, 10e4)
+
+	self.playbackState = Enum.PlaybackState.Paused
+	self.func = nil
+	task.wait(seconds)
+	self:Play()
 end
 
 function rainbow:Stop()
-	self.isPlaying = false
-	self.isPaused = false
-	self.finished = true
-	RunService:UnbindFromRenderStep("RainbowEffect" ..self.item.Name)
+	if self.playbackState ~= Enum.PlaybackState.Playing then
+		return
+	end
+
+	event3:Fire(self.item, self.playbackState)
+	self.playbackState = Enum.PlaybackState.Paused
+	self.func = nil
 end
 
 return rainbow
